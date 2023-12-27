80c5e5ea2213fbbfc4341ea20c33695334b13805
AnnaBanana0101 <77004422+AnnaBanana0101@users.noreply.github.com>
2023-12-27 16:33:52 +0530

    Implemented UC2

--- WordCount.java
+++ WordCount.java
@@ -5,10 +5,15 @@ public class WordCount {
     public static void main(String[] args) throws Exception {
         System.out.println("WordCount");
 
-        String text = "To be or not to be";
+        String text = "Paranoids are not\r\n" + //
+                "paranoid because they are paranoid but\r\n" + //
+                "because they keep putting themselves\r\n" + //
+                "deliberately into paranoid avoidable\r\n" + //
+                "situations";
         String[] words = format(text);
 
         HashMap<String, Integer> map = new HashMap<>();
+        Map<Integer, Integer> wordIndex = indexWords(words);
 
         for(String word: words)
         {
@@ -17,7 +22,21 @@ public class WordCount {
 
         for(Map.Entry<String, Integer> entry: map.entrySet())
         {
-            System.out.println("Key: " + entry.getKey() + " Value:" + entry.getValue());
+            System.out.println("Key: " + entry.getKey() +"\t"+ " Value:" + entry.getValue());
+        }
+
+        String searchWord = "not";
+        int hash_code = searchWord.hashCode();
+
+        if(wordIndex.containsKey(hash_code))
+        {
+            int index = wordIndex.get(hash_code);
+            System.out.println(searchWord + ": " + index);
+
+        }
+        else
+        {
+            System.out.println("Not found");
         }
 
     }
@@ -25,9 +44,25 @@ public class WordCount {
     public static String[] format(String text)
     {
         String[] result = text.replace(".", "")
+        .replace("?", "")
         .toLowerCase()
-            .split(" ");
+            .split("\\s");
 
         return result;
+    }
+
+    public static Map<Integer, Integer> indexWords(String[]words)
+    {
+        Map<Integer, Integer> wordIndex = new HashMap<>();
+
+        for(int i=0; i<words.length; i++)
+        {
+            String word = words[i];
+            int hash_code = word.hashCode();
+
+            wordIndex.put(hash_code, i);
+        }
+
+        return wordIndex;
     }
 }
